1eb4ac430fcc6e68408f01c8a89f52050b3bc080
Jean Viana <jeanfviana@gmail.com>
2023-10-10 00:38:57 -0300

    Alterações na automação

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+    });
+    
+    
+})
