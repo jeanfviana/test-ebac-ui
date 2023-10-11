027b8c25f980684eb989a0fed673aacc92e86728
Jean Viana <jeanfviana@gmail.com>
2023-10-11 01:31:52 -0300

    Alterações na automação

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,29 @@ context('Funcionalidade login', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    
     });
     
+    it('Deve exibir uma mensagem de erro ao inserir usúario invalidos', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_eac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido')
+
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha invalidos', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.co')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'a senha fornecida para o e-mail')
+        
+    });    
     
 })
